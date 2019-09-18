763ae7f8e9c6d8094e0adf682784a9306459289d
YanhuiJessica <137126578@qq.com>
2019-09-18 22:15:41 +0800

    完善注册检查 用户主页导航栏

--- views/login_register.py
+++ views/login_register.py
@@ -1,8 +1,36 @@
 from flask import Blueprint, render_template, redirect
 from forms import RegisterForm,LoginForm
+import re, logging
 
 login_register = Blueprint('login_register',__name__)
+username_pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9]+')
 
 @login_register.route('/')
 def get_login_register():
     return render_template('login_register.html',login_form=LoginForm(),register_form=RegisterForm())
+
+@login_register.route('/', methods=['POST'])
+def post_login_register():
+    login_form = LoginForm()
+    signin_form = RegisterForm()
+
+    if(login_form.login_sub.data):
+        try:
+            assert login_form.validate_on_submit(), '无效填写'
+        except AssertionError as e:
+            message = e.args[0] if len(e.args) else str(e)
+
+    if(signin_form.signin_sub.data):
+        try:
+            assert signin_form.validate_on_submit(), '无效填写'
+            email = signin_form.email.data
+            user = signin_form.username.data
+            assert username_pattern.fullmatch(user), '用户名只能包含汉字、英文及数字'
+            password = signin_form.password.data
+            confirm_passwd = signin_form.confirm_password.data
+            assert len(password) < 6, '密码长度至少为6位'
+            assert password != confirm_passwd, '两次密码不相等'
+        except AssertionError as e:
+            message = e.args[0] if len(e.args) else str(e)
+
+    return render_template('login_register.html',login_form=login_form,register_form=signin_form, message=message)
