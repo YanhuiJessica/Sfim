3958101e61530a1527a653e5641b6638500d48f7
YanhuiJessica <137126578@qq.com>
2019-09-18 22:15:41 +0800

    密码强度实时判断,注册页面前后端交互

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, FileField,HiddenField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 
 class PasswordForm(FlaskForm):
@@ -9,7 +9,9 @@ class RegisterForm(PasswordForm):
     email=StringField('email', validators=[DataRequired(),Email()])
     username = StringField('username', validators=[DataRequired()])
     confirm_password = PasswordField('confirm_password', validators=[DataRequired()])
+    signin_sub = SubmitField('Register')
 
 class LoginForm(PasswordForm):
     email=StringField('email', validators=[DataRequired(),Email()])
     password = PasswordField('password', validators=[DataRequired()])
+    login_sub = SubmitField('Login')
