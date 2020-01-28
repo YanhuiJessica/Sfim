8ae967fd3f0f7f734a2a698673b4020496e9be58
YanhuiJessica <137126578@qq.com>
2020-01-28 20:35:06 +0800

    完善，优化登录注册页加载速度

--- forms.py
+++ forms.py
@@ -1,17 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
+from flask import current_app as app
 
 class PasswordForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
 
+    def get_password_hash(self):
+        from passlib.hash import argon2
+        hashword = argon2.using(rounds = 256, memory_cost = 1024).hash(self.password.data)
+        app.logger.debug("hashword = %s",hashword)
+        return hashword
+
 class RegisterForm(PasswordForm):
-    email=StringField('email', validators=[DataRequired(),Email()])
+    email = StringField('email', validators=[DataRequired(),Email()])
     username = StringField('username', validators=[DataRequired()])
     confirm_password = PasswordField('confirm_password', validators=[DataRequired()])
     signin_sub = SubmitField('Register', validators=[DataRequired()])
 
 class LoginForm(PasswordForm):
-    email=StringField('email', validators=[DataRequired(),Email()])
+    email = StringField('email', validators=[DataRequired(),Email()])
     password = PasswordField('password', validators=[DataRequired()])
     login_sub = SubmitField('Login', validators=[DataRequired])
--- views/login_register.py
+++ views/login_register.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, redirect
 from forms import RegisterForm,LoginForm
 import re, logging
+from flask import current_app as app
 
 login_register = Blueprint('login_register',__name__)
-username_pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9]+')
+username_pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9_]+')
 
 @login_register.route('/')
 def get_login_register():
@@ -17,6 +18,7 @@ def post_login_register():
     if(login_form.login_sub.data):
         try:
             assert login_form.validate_on_submit(), '无效填写'
+            email = login_form.email.data
         except AssertionError as e:
             message = e.args[0] if len(e.args) else str(e)
 
@@ -25,12 +27,13 @@ def post_login_register():
             assert signin_form.validate_on_submit(), '无效填写'
             email = signin_form.email.data
             user = signin_form.username.data
-            assert username_pattern.fullmatch(user), '用户名只能包含汉字、英文及数字'
+            assert username_pattern.fullmatch(user), '用户名只能包含汉字、英文、数字及下划线'
+            assert len(user) <= 15, '用户名长度不得超过15个字符'
             password = signin_form.password.data
             confirm_passwd = signin_form.confirm_password.data
-            assert len(password) < 6, '密码长度至少为6位'
-            assert password != confirm_passwd, '两次密码不相等'
+            assert len(password) >= 6, '密码长度至少为6位'
+            assert password == confirm_passwd, '两次密码不相等'
+            hash_password = signin_form.get_password_hash()
         except AssertionError as e:
             message = e.args[0] if len(e.args) else str(e)
-
-    return render_template('login_register.html',login_form=login_form,register_form=signin_form, message=message)
+            return render_template('login_register.html',login_form=login_form,register_form=signin_form, message=message)
