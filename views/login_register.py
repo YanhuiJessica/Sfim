ba60178daf0462d09af28ce98c954094d9c861ae
YanhuiJessica <137126578@qq.com>
2020-01-31 22:51:51 +0800

    返回信息

--- views/login_register.py
+++ views/login_register.py
@@ -34,6 +34,9 @@ def post_login_register():
             assert len(password) >= 6, '密码长度至少为6位'
             assert password == confirm_passwd, '两次密码不相等'
             hash_password = signin_form.get_password_hash()
+            return render_template('login_register.html', login_form = login_form, register_form = signin_form, \
+                message = "注册成功！离使用 Sfim 只差一步！请前往邮箱激活您的账户。")
         except AssertionError as e:
             message = e.args[0] if len(e.args) else str(e)
-            return render_template('login_register.html',login_form=login_form,register_form=signin_form, message=message)
+            return render_template('login_register.html', login_form = login_form, register_form = signin_form, \
+                message = message)
