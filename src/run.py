347843483f4f844a06047f9d1fb42aeaf2e89978
YanhuiJessica <137126578@qq.com>
2020-02-02 18:11:47 +0800

    位置变更

--- src/run.py
+++ src/run.py
@@ -9,10 +9,11 @@ if __name__ == '__main__':
 
     # --------- 注册蓝图 --------- #
     app.register_blueprint(login_register, url_prefix='/login_register')
+    app.register_blueprint(verify, url_prefix='/verify')
 
     app.logger.addHandler(logging.StreamHandler())
     app.logger.setLevel(logging.DEBUG)
     #Level: DEBUG < INFO < WARING < ERROR < CRITICAL
     #大于等于设置Level级别的日志记录会被输出
 
-    app.run()
+    app.run(host="192.168.56.1", port=8080)
--- views/__init__.py
+++
@@ -1,1 +0,0 @@
-from .login_register import login_register
