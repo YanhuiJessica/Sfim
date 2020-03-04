96d4147cd3a0e98301c0a4ab568f5dea0943a3ce
YanhuiJessica <137126578@qq.com>
2020-03-04 22:07:45 +0800

    convert_bytes func

---
+++ src/models/file.py
@@ -0,0 +1,8 @@
+
+
+def convert_bytes(num):
+    # convert bytes to MB.... GB... etc
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
