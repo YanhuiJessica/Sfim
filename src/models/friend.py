e1da0bc1ed64302b27879182dbffa9c37fcebae1
YanhuiJessica <137126578@qq.com>
2020-04-23 23:05:14 +0800

    添加好友

--- src/models/friend.py
+++ src/models/friend.py
@@ -11,5 +11,26 @@ class Friend(db.Model):
     friendid = Column(INTEGER(11), primary_key=True)
 
     @classmethod
+    def get_by(cls, **kwargs):
+        return cls.query.filter_by(**kwargs).first()
+
+    @classmethod
     def find_user(cls, search_info):
         return User.get_like(search_info)
+
+    @classmethod
+    def add_friends(cls, usrid, fmail):
+        friendid = User.get_by(mail = fmail).usrid
+        friendship = Friend(usrid = usrid, friendid = friendid)
+        db.session.add(friendship)
+        db.session.commit()
+
+    @classmethod
+    def is_friends(cls, usrid, fmail):
+        friendid = User.get_by(mail = fmail).usrid
+        flag = cls.get_by(usrid = usrid, friendid = friendid) or \
+            cls.get_by(usrid = friendid, friendid = usrid)
+        if flag is None:
+            return False
+        else:
+            return True
--- src/views/search_result.py
+++ src/views/search_result.py
@@ -1,14 +1,37 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, flash, redirect
 from flask_login import login_required, current_user
 from models.friend import Friend
 
 search_result = Blueprint('search_result', __name__)
 
-@search_result.route('', methods=['GET'])
+@search_result.route('')
 @login_required
-def search_user():
+def all_users():
+    user = current_user
+    result = Friend.find_user('')
+    return render_template('search_result.html', user=user, users=result)
+
+@search_result.route('/', methods=['GET'])
+@login_required
+def manage_request():
     user = current_user
     search_info = request.args.get('search_str')
-    result = Friend.find_user(search_info)
-    return render_template('search_result.html', user=user, users=result, \
-        search_str=search_info, total=len(result))
+    if search_info is not None:
+        result = Friend.find_user(search_info)
+        return render_template('search_result.html', user=user, users=result, \
+            search_str=search_info, total=len(result))
+    else:
+        return add_user()
+
+def add_user():
+    user = current_user
+    fmail = request.args.get('umail')
+    plus = request.args.get('plus')
+    if user.mail == fmail:
+        flash('您不能添加自己！')
+    elif Friend.is_friends(user.usrid, fmail):
+        flash('您与 ' + fmail + ' 已是好友，不能重复添加！')
+    else:
+        Friend.add_friends(user.usrid, fmail)
+        flash('添加好友成功！')
+    return redirect('/search_result')
