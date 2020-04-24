e0b2fcbf04c7411684cd0b676cfdcc69867f6152
YanhuiJessica <137126578@qq.com>
2020-04-24 21:50:58 +0800

    好友列表显示

--- src/models/friend.py
+++ src/models/friend.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column
+from sqlalchemy import Column, or_
 from sqlalchemy.dialects.mysql import INTEGER
 from sqlalchemy.sql import func
 from database import db
@@ -15,21 +15,40 @@ class Friend(db.Model):
         return cls.query.filter_by(**kwargs).first()
 
     @classmethod
+    def del_by(cls, **kwargs):
+        return cls.query.filter_by(**kwargs).delete()
+
+    @classmethod
     def find_user(cls, search_info):
         return User.get_like(search_info)
 
     @classmethod
+    def get_friendship(cls, usrid):
+        return cls.query.filter(or_(Friend.usrid == usrid, Friend.friendid == usrid)).all()
+
+    @classmethod
     def add_friends(cls, usrid, fmail):
         friendid = User.get_by(mail = fmail).usrid
+        if usrid > friendid:
+            usrid, friendid = friendid, usrid
         friendship = Friend(usrid = usrid, friendid = friendid)
         db.session.add(friendship)
         db.session.commit()
 
     @classmethod
+    def del_friends(cls, usrid, fmail):
+        friendid = User.get_by(mail = fmail).usrid
+        if usrid < friendid:
+            cls.del_by(usrid = usrid, friendid = friendid)
+        else:
+            cls.del_by(usrid = friendid, friendid = usrid)
+
+    @classmethod
     def is_friends(cls, usrid, fmail):
         friendid = User.get_by(mail = fmail).usrid
-        flag = cls.get_by(usrid = usrid, friendid = friendid) or \
-            cls.get_by(usrid = friendid, friendid = usrid)
+        if usrid > friendid:
+            usrid, friendid = friendid, usrid
+        flag = cls.get_by(usrid = usrid, friendid = friendid)
         if flag is None:
             return False
         else:
--- src/views/friends.py
+++ src/views/friends.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect
 from flask_login import login_required, current_user
+from models.friend import Friend, User
 
 friends = Blueprint('friends', __name__)
 
@@ -7,4 +8,21 @@ friends = Blueprint('friends', __name__)
 @login_required
 def list_group():
     user = current_user
-    return render_template('friends.html', user=user)
+    uid = user.usrid
+    friendships = Friend.get_friendship(user.usrid)
+    friends = []
+    for fs in friendships:
+        if fs.usrid == uid:
+            friends.append(User.get_by(usrid=fs.friendid))
+        else:
+            friends.append(User.get_by(usrid=fs.usrid))
+    return render_template('friends.html', user=user, total=len(friends), friends=friends)
+
+@friends.route('/delete', methods=['GET'])
+@login_required
+def break_friendship():
+    user = current_user
+    fmail = request.args.get('fmail')
+    Friend.del_friends(user.usrid, fmail)
+    flash('好友 ' + fmail + '删除成功！')
+    return redirect('/friends')
