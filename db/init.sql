cbca9cbf05895650c55f85290906e06f7e221c4b
YanhuiJessica <137126578@qq.com>
2020-05-05 23:08:22 +0800

    更新数据库建表

--- db/init.sql
+++ db/init.sql
@@ -39,4 +39,38 @@ CREATE TABLE `files` (
   `uid` int(11) NOT NULL DEFAULT '0',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`fileid`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping structure for table sfim.share
+CREATE TABLE `share` (
+  `id_` int(11) NOT NULL AUTO_INCREMENT,
+  `fid` int(11) NOT NULL,
+  `share_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `enc_key` varbinary(1023) NOT NULL,
+  `sharekey` char(255) NOT NULL DEFAULT '',
+  `enc_sharekey` varbinary(2047) NOT NULL DEFAULT 'X',
+  `nonce` char(8) NOT NULL,
+  PRIMARY KEY (`id_`),
+  KEY `share_ibfk_1` (`fid`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- Dumping structure for table sfim.friends
+CREATE TABLE `friends` (
+  `usrid` int(11) NOT NULL DEFAULT '0',
+  `friendid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`usrid`,`friendid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping structure for table sfim.message
+CREATE TABLE `message` (
+  `id_` int(11) NOT NULL AUTO_INCREMENT,
+  `send_to_user` int(11) NOT NULL,
+  `send_from_user` varchar(255) NOT NULL DEFAULT '',
+  `shareid` int(11) NOT NULL,
+  `filename` varchar(255) DEFAULT NULL,
+  `read_status` tinyint(1) NOT NULL DEFAULT '0',
+  `send_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_`),
+  KEY `shareid` (`shareid`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
